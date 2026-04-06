2ccb0f3d9361416da4dc712e8689c9a1f028da08
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2026-04-06 17:56:16 +1000

    feature:create_FAB_add_task_on_TasksScreen

--- app/src/main/java/com/egorpoprotskiy/solobase/ui/tasks/TasksScreen.kt
+++ app/src/main/java/com/egorpoprotskiy/solobase/ui/tasks/TasksScreen.kt
@@ -8,19 +8,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.egorpoprotskiy.solobase.R
 import com.egorpoprotskiy.solobase.ui.tasks.components.TaskItem
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,6 +44,9 @@ fun TasksScreen(
 ) {
     // Подписываемся на список задач
     val tasks by viewModel.tasks.collectAsStateWithLifecycle()
+    //переменная для диалогового окна создания новой задачи
+    var showDialog by remember() { mutableStateOf(false) }
+    var taskText by remember() { mutableStateOf("")}
     // Базовая обертка для экрана
     Scaffold(
         topBar = {
@@ -41,6 +57,18 @@ fun TasksScreen(
                     titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {showDialog = true},
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_large)),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.add_new_task)
+                )
+            }
         }
     ) { paddingValues ->
         // Проверяем, есть ли задачи
@@ -70,6 +98,51 @@ fun TasksScreen(
                     )
                 }
             }
+        }
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDialog = false
+                    taskText = ""},
+                title = {
+                    Text(
+                        stringResource(R.string.add_new_task))
+
+                },
+                text = {
+                    OutlinedTextField(
+                        value = taskText,
+                        onValueChange = { taskText = it}, // Обновляем состояние при наборе
+                        label = { Text("Что нужно сделать")},
+//                        modifier = Modifier.fillMaxSize(),
+                        singleLine = false
+                    )
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            if (taskText.isNotBlank()) {
+                                showDialog = false
+                                taskText = ""
+                            }
+                        }
+                    ) {
+                        Text(
+                            stringResource(R.string.add_new_task)
+                        )
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                            taskText = ""
+                        }
+                    ) {
+                        Text(stringResource(R.string.cancel_button))
+                    }
+                }
+            )
         }
     }
 }
