1f6b4565bdf6c0702dd08fd0df6a61d251cac4fe
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2026-04-26 17:15:42 +1000

    Добавлены анимации с задачами и переключение на матрицу

--- app/src/main/java/com/egorpoprotskiy/solobase/ui/tasks/TasksScreen.kt
+++ app/src/main/java/com/egorpoprotskiy/solobase/ui/tasks/TasksScreen.kt
@@ -1,12 +1,17 @@
 package com.egorpoprotskiy.solobase.ui.tasks
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -40,8 +45,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -65,15 +70,21 @@ fun TasksScreen(
     val displayMode by viewModel.displayMode.collectAsState()
     //переменная для диалогового окна создания новой задачи
     var showDialog by remember() { mutableStateOf(false) }
-    var taskText by remember() { mutableStateOf("")}
+    var taskText by remember() { mutableStateOf("") }
     //Диалоговое окно на удаление задачи
-    var showDeleteDialog by remember() { mutableStateOf(false)}
-    var taskToDelete by remember() { mutableStateOf<Task?>(null)}
+    var showDeleteDialog by remember() { mutableStateOf(false) }
+    var taskToDelete by remember() { mutableStateOf<Task?>(null) }
     // Разделение экрана кнопкой на общий список задач и на матрицу
     var isMatrixMode by remember { mutableStateOf(false) } // По умолчанию — обычный список
     // Отслеживание состояний isUrgent и isImportant
     var isUrgent by remember { mutableStateOf(false) }
     var isImportant by remember { mutableStateOf(false) }
+    //Переменная для анимации(чтобы иконка плавно вращалась)
+    val rotation by animateFloatAsState(
+        targetValue = if (displayMode == TasksDisplayMode.MATRIX) 180f else 0f,
+        animationSpec = spring(stiffness = Spring.StiffnessLow),
+        label = "IconRotation"
+    )
     // Базовая обертка для экрана
     Scaffold(
         topBar = {
@@ -90,10 +101,11 @@ fun TasksScreen(
                             imageVector = if (displayMode == TasksDisplayMode.LIST)
                                 Icons.Default.GridView else Icons.Default.List,
                             contentDescription = stringResource(R.string.change_mode),
-                                    // Явно задаем размер, чтобы иконка не казалась мелкой
-                            modifier = Modifier.size(24.dp),
+                            modifier = Modifier.rotate(rotation) // Добавляем вращение
+                            // Явно задаем размер, чтобы иконка не казалась мелкой
+//                            modifier = Modifier.size(24.dp),
                             // Используем onPrimary, так как фон шапки у нас SoloGreen (Primary)
-                            tint = MaterialTheme.colorScheme.onPrimary
+//                            tint = MaterialTheme.colorScheme.onPrimary
                         )
                     }
                 },
@@ -106,7 +118,7 @@ fun TasksScreen(
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {showDialog = true},
+                onClick = { showDialog = true },
                 modifier = Modifier.padding(dimensionResource(R.dimen.padding_large)),
                 shape = MaterialTheme.shapes.large,
                 containerColor = MaterialTheme.colorScheme.secondary,
@@ -119,45 +131,59 @@ fun TasksScreen(
             }
         }
     ) { paddingValues ->
-        // Проверяем, есть ли задачи
-//        if (tasks.isEmpty()) {
-//            EmptyState(modifier = Modifier.padding(paddingValues))
-        if (displayMode == TasksDisplayMode.MATRIX) {
-            EisenhowerMatrix(
-                tasks = tasks,
-                // Вот решение твоей "красной" ошибки:
-                onTaskChecked = { task, isCompleted ->
-                    viewModel.onTaskChecked(task, isCompleted) // Проверь название метода во ViewModel!
-                },
-                modifier = Modifier.padding(paddingValues)
-            )
-        } else {
-            // Список задач
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
-//                contentPadding = PaddingValues(16.dp),
-//                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                items(
-                    items = tasks,
-                    key = { it.id } // Важно для оптимизации анимаций и прокрутки
-                ) { task ->
-                    TaskItem(
-                        task = task,
-                        onCheckedChange = { isChecked ->
-                            viewModel.onTaskChecked(task, isChecked)
-                        },
-                        onClick = {
+        //Анимация переключения между списком задач и матрицей.
+        AnimatedContent(
+            targetState = displayMode,
+            label = "ModeAnimation",
+            transitionSpec = {
+                fadeIn(animationSpec = tween(400)) + scaleIn(initialScale = 0.92f) togetherWith
+                        fadeOut(animationSpec = tween(300))
+            }
+        ) { targetMode ->
+            if (targetMode == TasksDisplayMode.MATRIX) {
+                EisenhowerMatrix(
+                    tasks = tasks,
+                    // Вот решение твоей "красной" ошибки:
+                    onTaskChecked = { task, isCompleted ->
+                        viewModel.onTaskChecked(
+                            task,
+                            isCompleted
+                        ) // Проверь название метода во ViewModel!
+                    },
+                    modifier = Modifier.padding(paddingValues)
+                )
+            } else {
+                // Список задач
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                ) {
+                    items(
+                        items = tasks,
+                        key = { it.id } // Важно для оптимизации анимаций и прокрутки
+                    ) { task ->
+                        TaskItem(
+                            task = task,
+                            //анимация в списке(плавное добавление и удаление задачи)
+                            modifier = Modifier.animateItem(
+                                fadeInSpec = tween(300),
+                                fadeOutSpec = tween(300),
+                                placementSpec = spring(stiffness = Spring.StiffnessLow) // Плавное перемещение
+                            ),
+                            onCheckedChange = { isChecked ->
+                                viewModel.onTaskChecked(task, isChecked)
+                            },
+                            onClick = {
 //                        viewModel.onTaskClicked(task)
-                        },
-                        onDeleteClick = {
-                            showDeleteDialog = true
-                            taskToDelete = task
+                            },
+                            onDeleteClick = {
+                                showDeleteDialog = true
+                                taskToDelete = task
 //                            viewModel.deleteTask(task.id)
-                        }
-                    )
+                            }
+                        )
+                    }
                 }
             }
         }
@@ -169,14 +195,13 @@ fun TasksScreen(
                     isUrgent = false // Сбрасываем при закрытии
                     isImportant = false
                 },
-                title = { Text(stringResource(R.string.add_new_task))},
+                title = { Text(stringResource(R.string.add_new_task)) },
                 text = {
                     Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
                         OutlinedTextField(
                             value = taskText,
                             onValueChange = { taskText = it }, // Обновляем состояние при наборе
                             label = { Text(stringResource(R.string.task_input_label)) },
-//                          modifier = Modifier.fillMaxSize(),
                             singleLine = false
                         )
                         FlowRow(
@@ -188,7 +213,13 @@ fun TasksScreen(
                             FilterChip(
                                 selected = isUrgent,
                                 onClick = { isUrgent = !isUrgent },
-                                label = { Text("Срочно", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center) },
+                                label = {
+                                    Text(
+                                        "Срочно",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                },
                                 leadingIcon = { Icon(Icons.Default.Bolt, null) },
                                 modifier = Modifier.weight(1f), // Занимает 50% ширины
                                 colors = FilterChipDefaults.filterChipColors(
@@ -201,7 +232,13 @@ fun TasksScreen(
                             FilterChip(
                                 selected = isImportant,
                                 onClick = { isImportant = !isImportant },
-                                label = { Text("Важно", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center) },
+                                label = {
+                                    Text(
+                                        "Важно",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                },
                                 leadingIcon = { Icon(Icons.Default.Star, null) },
                                 modifier = Modifier.weight(1f), // Занимает остальные 50%
                                 colors = FilterChipDefaults.filterChipColors(
@@ -278,7 +315,8 @@ fun TasksScreen(
                     ) {
                         Text(
                             stringResource(R.string.delete_button),
-                            color = MaterialTheme.colorScheme.error)
+                            color = MaterialTheme.colorScheme.error
+                        )
                     }
                 },
                 dismissButton = {
@@ -291,28 +329,6 @@ fun TasksScreen(
                         Text(stringResource(R.string.cancel_button))
                     }
                 }
-            )
-        }
-    }
-}
-
-
-@Composable
-fun EmptyState(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = stringResource(R.string.tasks_empty_title),
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-            Text(
-                text = stringResource(R.string.tasks_empty_subtitle),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
