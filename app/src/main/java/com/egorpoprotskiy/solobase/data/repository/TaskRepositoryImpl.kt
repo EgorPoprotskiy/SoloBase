def5d53ff8d2f4da4e31ab4fc279131b9d0ce5c5
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2026-06-20 16:44:54 +1000

    clean TaskRepositoryImpl

--- app/src/main/java/com/egorpoprotskiy/solobase/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/egorpoprotskiy/solobase/data/repository/TaskRepositoryImpl.kt
@@ -6,17 +6,13 @@ import com.egorpoprotskiy.solobase.data.local.mapper.toEntity
 import com.egorpoprotskiy.solobase.domain.models.Task
 import com.egorpoprotskiy.solobase.domain.repository.TaskRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class TaskRepositoryImpl @Inject constructor(
     private val taskDao: TaskDao // Hilt сам подставит его сюда
 ) : TaskRepository {
-    private val tasksFlow = MutableStateFlow<List<Task>>(emptyList())
-//    override fun getTasks(): Flow<List<Task>> = flowOf(emptyList())
-//    override fun getTasks(): Flow<List<Task>> = tasksFlow
+
     override fun getTasks(): Flow<List<Task>> = taskDao.getAllTasks()
         .map { tasks -> tasks.map { it.toDomain() } }
 
@@ -28,24 +24,9 @@ class TaskRepositoryImpl @Inject constructor(
         TODO("Not yet implemented")
     }
 
-//    override suspend fun addTask(task: Task) {
-//        tasksFlow.update { currentList ->
-//            currentList + task
-//        }
-//    }
     override suspend fun addTask(task: Task) = taskDao.insertTask(task.toEntity())
 
-//    override suspend fun updateTask(task: Task) {
-//        tasksFlow.update { list ->
-//            list.map { if (it.id == task.id) task else it }
-//        }
-//    }
     override suspend fun updateTask(task: Task) = taskDao.updateTask(task.toEntity())
 
-//    override suspend fun deleteTask(id: String) {
-//        tasksFlow.update { list ->
-//            list.filter { it.id != id }
-//        }
-//    }
     override suspend fun deleteTask(id: String) = taskDao.deleteTaskById(id)
 }
